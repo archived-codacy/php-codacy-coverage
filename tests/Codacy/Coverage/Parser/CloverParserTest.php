845426edbf5129b51ca8c5fe4ced46c44fb5058f
Rafael Cortês <mrfyda@gmail.com>
2015-03-19 15:21:22 +0000

    remove slash escaping

--- tests/Codacy/Coverage/Parser/CloverParserTest.php
+++ tests/Codacy/Coverage/Parser/CloverParserTest.php
@@ -5,7 +5,6 @@ use Codacy\Coverage\Parser\CloverParser;
 class CloverParserTest extends PHPUnit_Framework_TestCase
 {
 
-
     public function testThrowsExceptionOnWrongPath()
     {
         $this->setExpectedException('InvalidArgumentException');
@@ -17,8 +16,8 @@ class CloverParserTest extends PHPUnit_Framework_TestCase
      */
     public function testCanParseCloverXmlWithoutProject()
     {
-		$this->_canParseClover('tests/res/clover/clover.xml', "/home/jacke/Desktop/codacy-php");
-	}
+        $this->_canParseClover('tests/res/clover/clover.xml', "/home/jacke/Desktop/codacy-php");
+    }
 
     /**
      * Testing against the clover coverage report 'tests/res/clover/clover.xml'
@@ -26,37 +25,37 @@ class CloverParserTest extends PHPUnit_Framework_TestCase
      * as 2nd (ootional) parameter. Otherwise the filename will not be correct and test
      * the would file on other machines or in other directories.
      */
-	public function testCanParseCloverXmlWithProject()
+    public function testCanParseCloverXmlWithProject()
     {
-		$this->_canParseClover('tests/res/clover/clover_without_packages.xml', "/home/jacke/Desktop/codacy-php");
-	}
+        $this->_canParseClover('tests/res/clover/clover_without_packages.xml', "/home/jacke/Desktop/codacy-php");
+    }
 
     private function _canParseClover($path, $rootDir)
     {
-		$parser = new CloverParser($path, $rootDir);
-		$report = $parser->makeReport();
-		$this->assertEquals(38, $report->getTotal());
-		$this->assertEquals(5, sizeof($report->getFileReports()));
-		
-		$parserFileReports = $report->getFileReports();
+        $parser = new CloverParser($path, $rootDir);
+        $report = $parser->makeReport();
+        $this->assertEquals(38, $report->getTotal());
+        $this->assertEquals(5, sizeof($report->getFileReports()));
+
+        $parserFileReports = $report->getFileReports();
 
         $parserFileReport = $parserFileReports[0];
         $coverageReportFileReport = $parserFileReports[1];
-		
-		$this->assertEquals(33, $parserFileReport->getTotal());
-		$this->assertEquals(33, $coverageReportFileReport->getTotal());
-		
-		$parserFileName = $parserFileReport->getFileName();	
-		
-		$reportFileName = $coverageReportFileReport->getFileName();
+
+        $this->assertEquals(33, $parserFileReport->getTotal());
+        $this->assertEquals(33, $coverageReportFileReport->getTotal());
+
+        $parserFileName = $parserFileReport->getFileName();
+
+        $reportFileName = $coverageReportFileReport->getFileName();
 
         $fileReports = $report->getFileReports();
         $fileReport = $fileReports[1];
 
         $expLineCoverage = array(11 => 1, 12 => 1, 13 => 1, 16 => 1);
         $this->assertEquals($fileReport->getLineCoverage(), $expLineCoverage);
-				
-		$this->assertEquals("src/Codacy/Coverage/Parser/Parser.php", $parserFileName);
-		$this->assertEquals("src/Codacy/Coverage/Report/CoverageReport.php", $reportFileName);
-	}
+
+        $this->assertEquals("src/Codacy/Coverage/Parser/Parser.php", $parserFileName);
+        $this->assertEquals("src/Codacy/Coverage/Report/CoverageReport.php", $reportFileName);
+    }
 }
