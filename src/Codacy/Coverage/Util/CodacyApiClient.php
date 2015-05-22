12db21569c5af2ce1c4ea5e2f8787b26dfd46ba7
Rafael Cortês <mrfyda@gmail.com>
2015-05-22 23:19:11 +0100

    update to api 2.0

--- src/Codacy/Coverage/Util/CodacyApiClient.php
+++ src/Codacy/Coverage/Util/CodacyApiClient.php
@@ -9,22 +9,33 @@ namespace Codacy\Coverage\Util;
 class CodacyApiClient
 {
 
+    function __construct($baseUrl, $projectToken)
+    {
+        $this->baseUrl = $baseUrl;
+        $this->projectToken = $projectToken;
+    }
+
     /**
-     * @param string $url  url to post to
+     * @param string $commit commit uuid
      * @param string $data the JSON data
      *
-     * @return string      sucess message
+     * @return string      success message
      *
      * @throws \Exception   when remote server response
      */
-    static function postData($url, $data)
+    public function sendCoverage($commit, $data)
     {
+        $url = $this->baseUrl . "/api/2.0/coverage/" . $commit . "/php";
+
         $curl = curl_init($url);
         curl_setopt($curl, CURLOPT_HEADER, false);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         curl_setopt(
             $curl, CURLOPT_HTTPHEADER,
-            array("Content-type: application/json")
+            array(
+                "Content-type: application/json",
+                "project_token: " . $this->projectToken
+            )
         );
         curl_setopt($curl, CURLOPT_POST, true);
         curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
@@ -36,7 +47,7 @@ class CodacyApiClient
         if ($status < 200 || $status > 201) {
             throw new \Exception(
                 sprintf("Error: call to URL %s failed with status %s, response %s, curl_error %u",
-                         $url, $status, $json_response, curl_error($curl), curl_errno($curl)
+                    $url, $status, $json_response, curl_error($curl), curl_errno($curl)
                 )
             );
         }
